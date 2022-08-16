639a639f6fe6bb1478234bfe89b829bc0c27ea41
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-08-16 17:50:18 +0300

    step2

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -4,8 +4,10 @@ import { nanoid } from 'nanoid';
 
 export class Phonebook extends Component {
   state = {
-  contacts: [],
-  name: ''
+    contacts: [],
+    filter: '',
+    name: '',
+    number: ''
   }
 
   
@@ -15,7 +17,11 @@ export class Phonebook extends Component {
   
   onSubmit = (event) => {
     event.preventDefault();
-    this.setState({ contacts: [...this.state.contacts, this.state.name], name: '' });
+    this.setState({
+      contacts: [...this.state.contacts, [`${this.state.name}: ${this.state.number}`]],
+      name: '',
+      number: ''
+    });
   }
   
   render() {
@@ -29,18 +35,35 @@ export class Phonebook extends Component {
           <label>
             Name
             <Input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
               value={this.state.name}
+              onChange={this.onChange}
+            />
+          </label>
+          <label>
+            Number
+            <Input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.number}
               onChange={this.onChange}
             />
           </label>
           <button type="submit">Add contact</button>
         </Form>
         <h2>Contacts</h2>
+        <p>Find contacts by name</p>
+        <Input
+          type="text"
+          name="filter"
+        />
         <ul>
           {this.state?.contacts?.map(contact => (
             <li key={nanoid()}>{contact}</li>))}
