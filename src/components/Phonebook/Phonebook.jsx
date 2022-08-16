b1b4dd1815a6b6b61ecc2a963197e77db347a470
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-08-16 16:42:59 +0300

    step1

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
+import { nanoid } from 'nanoid';
 
 export class Phonebook extends Component {
   state = {
@@ -7,14 +8,24 @@ export class Phonebook extends Component {
   name: ''
   }
 
-  onChange = (event) => console.log(event);
+  
+
+  onChange = (event) => this.setState({ [event.target.name]: event.target.value });
+    // console.log(event.target.value);
+  
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ contacts: [...this.state.contacts, this.state.name], name: '' });
+  }
   
   render() {
+
+    console.log(this.state);
 
     return (
       <>
       <h1>Phonebook</h1>
-        <Form>
+        <Form onSubmit={this.onSubmit}>
           <label>
             Name
             <Input
@@ -23,12 +34,17 @@ export class Phonebook extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            value={this.state.name}
+              value={this.state.name}
+              onChange={this.onChange}
             />
           </label>
           <button type="submit">Add contact</button>
         </Form>
-      <h2>Contacts</h2>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state?.contacts?.map(contact => (
+            <li key={nanoid()}>{contact}</li>))}
+        </ul>
       </>
     )
   }
