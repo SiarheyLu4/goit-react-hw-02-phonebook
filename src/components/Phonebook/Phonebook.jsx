b3a8343a4f6eb442f05e22437fd50374aab95892
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-08-17 13:00:08 +0300

    17.08

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,31 +1,63 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
 import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix';
 
 export class Phonebook extends Component {
   state = {
-    contacts: [],
-    filter: '',
-    name: '',
-    number: ''
-  }
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+  name: '',
+  number: ''
+}
 
-  
+  onChangeFilter = (event) => this.setState({ filter: event.target.value});
 
-  onChange = (event) => this.setState({ [event.target.name]: event.target.value });
+  onChangeForm = (event) => this.setState({ [event.target.name]: event.target.value });
     // console.log(event.target.value);
   
   onSubmit = (event) => {
     event.preventDefault();
+    this.addContact(this.state);
     this.setState({
-      contacts: [...this.state.contacts, [`${this.state.name}: ${this.state.number}`]],
       name: '',
       number: ''
     });
   }
+
+  getFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    const caseInsensitive = filter.toLowerCase();
+    return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(caseInsensitive));
+  };
+
+  addContact = ({ name, number }) => {
+    this.setState(({ contacts }) => {
+      const isContact = contacts.find(contact => contact.name === name);
+
+      if (isContact) {
+        Notify.warning(`${name} is already in contact`);
+        // return contacts;
+      } else {
+        const newContact = {
+        id: nanoid(),
+        name,
+        number,
+        };
+        return {
+          contacts: [...contacts, newContact],
+          };
+      }
+    });
+  };
   
   render() {
-
     console.log(this.state);
 
     return (
@@ -41,7 +73,7 @@ export class Phonebook extends Component {
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               value={this.state.name}
-              onChange={this.onChange}
+              onChange={this.onChangeForm}
             />
           </label>
           <label>
@@ -53,20 +85,23 @@ export class Phonebook extends Component {
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
               value={this.state.number}
-              onChange={this.onChange}
+              onChange={this.onChangeForm}
             />
           </label>
           <button type="submit">Add contact</button>
         </Form>
         <h2>Contacts</h2>
-        <p>Find contacts by name</p>
-        <Input
-          type="text"
-          name="filter"
-        />
+        <label>Find contacts by name
+          <Input
+            type="text"
+            name="filter"
+            value={this.state.filter}
+            onChange={this.onChangeFilter}
+          />
+        </label>
         <ul>
-          {this.state?.contacts?.map(contact => (
-            <li key={nanoid()}>{contact}</li>))}
+          {this.getFilterContacts().sort((a, b) => a.name.localeCompare(b.name)).map(({id, name, number}) => (
+            <li key={id}>{name}: {number}</li>))}
         </ul>
       </>
     )
